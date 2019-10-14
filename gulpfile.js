6519b0829f64f7413cc2a82a6a21ec12866da983
zhangyl2 <zhangyl2@ziroom.com>
2019-10-14 15:47:18 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,11 @@
-/* Refrences:
-1. http://notes.iissnan.com/2016/publishing-github-pages-with-travis-ci
-2. https://github.com/chrisjlee/hexo-theme-zurb-foundation/blob/e82f45a82bbaaee063bcb1298cd9793575afb142/gulpfile.js
-3. https://github.com/gulpjs/gulp/blob/master/docs/recipes/delete-files-folder.md
-4. https://hexo.io/api/
-5. https://github.com/iissnan/theme-next-docs/blob/master/.travis.yml
-*/
-
 var gulp = require('gulp');
 var minifycss = require('gulp-clean-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 var imagemin = require('gulp-imagemin');
-var del = require('del');
-var runSequence = require('run-sequence');
-var Hexo = require('hexo');
-
-
-gulp.task('clean', function() {
-    return del(['public/**/*']);
-});
-
-// generate html with 'hexo generate'
-var hexo = new Hexo(process.cwd(), {});
-gulp.task('generate', function(cb) {
-    hexo.init().then(function() {
-        return hexo.call('generate', {
-            watch: false
-        });
-    }).then(function() {
-        return hexo.exit();
-    }).then(function() {
-        return cb()
-    }).catch(function(err) {
-        console.log(err);
-        hexo.exit(err);
-        return cb(err);
-    })
-})
-
-gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest('./public'));
-});
-
+ 
+// 压缩html
 gulp.task('minify-html', function() {
     return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
@@ -58,27 +17,30 @@ gulp.task('minify-html', function() {
         }))
         .pipe(gulp.dest('./public'))
 });
-
+// 压缩css
+gulp.task('minify-css', function() {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./public'));
+});
+// 压缩js
 gulp.task('minify-js', function() {
-    return gulp.src('./public/**/*.js')
+    return gulp.src('./public/js/**/*.js')
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
 });
-
-gulp.task('minify-img', function() {
-    return gulp.src('./public/images/*')
-        .pipe(imagemin())
+// 压缩图片
+gulp.task('minify-images', function() {
+    return gulp.src('./public/images/**/*.*')
+        .pipe(imagemin(
+        [imagemin.gifsicle({'optimizationLevel': 3}), 
+        imagemin.jpegtran({'progressive': true}), 
+        imagemin.optipng({'optimizationLevel': 7}), 
+        imagemin.svgo()],
+        {'verbose': true}))
         .pipe(gulp.dest('./public/images'))
-})
-
-gulp.task('compress', function(cb) {
-    runSequence(['minify-html', 'minify-css', 'minify-js', 'minify-img'], cb);
 });
-
-
-//gulp.task('build', ['clean', 'generate', 'compress']);
-gulp.task('build', function(cb) {
-    runSequence('clean', 'generate', 'compress', cb)
-});
-
-gulp.task('default', [])
+// 默认任务
+gulp.task('default',gulp.series(gulp.parallel('minify-html','minify-css','minify-js','minify-images')));
