976404cce1c3dd5c2badd5a2b0212d4418e82dce
zhangyl2 <zhangyl2@ziroom.com>
2019-10-14 18:04:52 +0800

    test

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,18 @@ var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
 var imagemin = require('gulp-imagemin');
- 
-// 压缩html
-gulp.task('minify-html', function() {
+var babel = require('gulp-babel');
+
+// 压缩css文件
+gulp.task('minify-css', function (done) {
+    return gulp.src('./public/**/*.css')
+        .pipe(minifycss())
+        .pipe(gulp.dest('./public'));
+    done();
+});
+
+// 压缩html文件
+gulp.task('minify-html', function (done) {
     return gulp.src('./public/**/*.html')
         .pipe(htmlclean())
         .pipe(htmlmin({
@@ -15,32 +24,59 @@ gulp.task('minify-html', function() {
             minifyCSS: true,
             minifyURLs: true,
         }))
-        .pipe(gulp.dest('./public'))
-});
-// 压缩css
-gulp.task('minify-css', function() {
-    return gulp.src('./public/**/*.css')
-        .pipe(minifycss({
-            compatibility: 'ie8'
-        }))
         .pipe(gulp.dest('./public'));
+    done();
 });
-// 压缩js
-gulp.task('minify-js', function() {
-    return gulp.src('./public/js/**/*.js')
+
+// 压缩js文件
+gulp.task('minify-js', function (done) {
+    return gulp.src(['./public/**/*.js', '!./public/**/*.min.js'])
+        .pipe(babel({
+            //将ES6代码转译为可执行的JS代码
+            presets: ['env'] // es5检查机制
+        }))
         .pipe(uglify())
         .pipe(gulp.dest('./public'));
+    done();
 });
-// 压缩图片
-gulp.task('minify-images', function() {
-    return gulp.src('./public/images/**/*.*')
-        .pipe(imagemin(
-        [imagemin.gifsicle({'optimizationLevel': 3}), 
-        imagemin.jpegtran({'progressive': true}), 
-        imagemin.optipng({'optimizationLevel': 7}), 
-        imagemin.svgo()],
-        {'verbose': true}))
-        .pipe(gulp.dest('./public/images'))
+
+// 压缩 public/images 目录内图片(Version<3)
+// gulp.task('minify-images', function () {
+//     gulp.src('./public/images/**/*.*')
+//         .pipe(imagemin({
+//             optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+//             progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+//             interlaced: false, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+//             multipass: false, //类型：Boolean 默认：false 多次优化svg直到完全优化
+//         }))
+//         .pipe(gulp.dest('./public/images'));
+// });
+
+// 压缩 public/images 目录内图片(Version>3)
+gulp.task('minify-images', function (done) {
+    gulp.src('./public/images/**/*.*')
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.jpegtran({progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(gulp.dest('./public/images'));
+    done();
 });
-// 默认任务
-gulp.task('default',gulp.series(gulp.parallel('minify-html','minify-css','minify-js','minify-images')));
+
+//4.0以前的写法 
+//gulp.task('default', [
+//  'minify-html', 'minify-css', 'minify-js', 'minify-images'
+//]);
+//4.0以后的写法
+// 执行 gulp 命令时执行的任务
+gulp.task('default', gulp.series(gulp.parallel('minify-html', 'minify-css', 'minify-js', 'minify-images')), function () {
+    console.log("----------gulp Finished----------");
+    // Do something after a, b, and c are finished.
+});
